73f39cd442d13029b3feb03ea2f373a419043e94
yashaswi maharshi <maha.yash@gmail.com>
2018-09-04 08:34:41 +0530

    UI changes as per the CSS module

--- Saanwariya_WS/src/components/header.js
+++ Saanwariya_WS/src/components/header.js
@@ -1,53 +1,66 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import styles from '../styles/main_styles.module.css'
+import bootstrapStyle from '../styles/bootstrap4/bootstrap.min.module.css'
 
 const Header = () => (
   <div className={styles.header}>
     <div className={styles.top_bar}>
-      <div class="container">
-        <div class="row">
-          <div class="col d-flex flex-row">
-            <div class="phone">+45 345 3324 56789</div>
-            <div class="social">
-              <ul class="social_list">
-                <li class="social_list_item">
+      <div className={bootstrapStyle.container}>
+        <div className={bootstrapStyle.row}>
+          <div
+            className={`${bootstrapStyle.col} ${bootstrapStyle['d-flex']} ${
+              bootstrapStyle['flex-row']
+            }`}
+          >
+            <div classname={styles.phone}>+45 345 3324 56789</div>
+            <div className={styles.social}>
+              <ul className={styles.social_list}>
+                <li className={styles.social_list_item}>
                   <a href="#">
-                    <i class="fa fa-pinterest" aria-hidden="true" />
+                    <i
+                      className={`${bootstrapStyle.fa} ${
+                        bootstrapStyle['fa-pinterest']
+                      } ${(bootstrapStyle['aria-hidden'] = 'true')}`}
+                    />
                   </a>
                 </li>
-                <li class="social_list_item">
+                <li className={styles.social_list_item}>
                   <a href="#">
-                    <i class="fa fa-facebook" aria-hidden="true" />
+                    <i
+                      className={`${bootstrapStyle.fa} ${
+                        bootstrapStyle['fa-facebook']
+                      } ${(bootstrapStyle['aria-hidden'] = 'true')}`}
+                    />
                   </a>
                 </li>
-                <li class="social_list_item">
+                <li className="social_list_item">
                   <a href="#">
-                    <i class="fa fa-twitter" aria-hidden="true" />
+                    <i className="fa fa-twitter" aria-hidden="true" />
                   </a>
                 </li>
-                <li class="social_list_item">
+                <li className="social_list_item">
                   <a href="#">
-                    <i class="fa fa-dribbble" aria-hidden="true" />
+                    <i className="fa fa-dribbble" aria-hidden="true" />
                   </a>
                 </li>
-                <li class="social_list_item">
+                <li className="social_list_item">
                   <a href="#">
-                    <i class="fa fa-behance" aria-hidden="true" />
+                    <i className="fa fa-behance" aria-hidden="true" />
                   </a>
                 </li>
-                <li class="social_list_item">
+                <li className="social_list_item">
                   <a href="#">
-                    <i class="fa fa-linkedin" aria-hidden="true" />
+                    <i className="fa fa-linkedin" aria-hidden="true" />
                   </a>
                 </li>
               </ul>
             </div>
-            <div class="user_box ml-auto">
-              <div class="user_box_login user_box_link">
+            <div className="user_box ml-auto">
+              <div className="user_box_login user_box_link">
                 <a href="#">login</a>
               </div>
-              <div class="user_box_register user_box_link">
+              <div className={`${styles.user_box_register} ${styles.user_box_link}`}>
                 <a href="#">register</a>
               </div>
             </div>
@@ -56,43 +69,43 @@ const Header = () => (
       </div>
     </div>
 
-    <nav class="main_nav">
-      <div class="container">
-        <div class="row">
-          <div class="col main_nav_col d-flex flex-row align-items-center justify-content-start">
-            <div class="logo_container">
-              <div class="logo">
+    <nav className="main_nav">
+      <div className="container">
+        <div className="row">
+          <div className="col main_nav_col d-flex flex-row align-items-center justify-content-start">
+            <div className="logo_container">
+              <div className="logo">
                 <a href="#">
                   <img src="images/logo.png" alt="" />travelix
                 </a>
               </div>
             </div>
-            <div class="main_nav_container ml-auto">
-              <ul class="main_nav_list">
-                <li class="main_nav_item">
+            <div className="main_nav_container ml-auto">
+              <ul className="main_nav_list">
+                <li className="main_nav_item">
                   <a href="#">home</a>
                 </li>
-                <li class="main_nav_item">
+                <li className="main_nav_item">
                   <a href="about.html">about us</a>
                 </li>
-                <li class="main_nav_item">
+                <li className="main_nav_item">
                   <a href="offers.html">offers</a>
                 </li>
-                <li class="main_nav_item">
+                <li className="main_nav_item">
                   <a href="blog.html">news</a>
                 </li>
-                <li class="main_nav_item">
+                <li className="main_nav_item">
                   <a href="contact.html">contact</a>
                 </li>
               </ul>
             </div>
-            {/* <div class="content_search ml-lg-0 ml-auto">
+            {/* <div className="content_search ml-lg-0 ml-auto">
 							<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 							width="17px" height="17px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
 								<g>
 									<g>
 										<g>
-											<path class="mag_glass" fill="#FFFFFF" d="M78.438,216.78c0,57.906,22.55,112.343,63.493,153.287c40.945,40.944,95.383,63.494,153.287,63.494
+											<path className="mag_glass" fill="#FFFFFF" d="M78.438,216.78c0,57.906,22.55,112.343,63.493,153.287c40.945,40.944,95.383,63.494,153.287,63.494
 											s112.344-22.55,153.287-63.494C489.451,329.123,512,274.686,512,216.78c0-57.904-22.549-112.342-63.494-153.286
 											C407.563,22.549,353.124,0,295.219,0c-57.904,0-112.342,22.549-153.287,63.494C100.988,104.438,78.439,158.876,78.438,216.78z
 											M119.804,216.78c0-96.725,78.69-175.416,175.415-175.416s175.418,78.691,175.418,175.416
@@ -101,7 +114,7 @@ const Header = () => (
 									</g>
 									<g>
 										<g>
-											<path class="mag_glass" fill="#FFFFFF" d="M6.057,505.942c4.038,4.039,9.332,6.058,14.625,6.058s10.587-2.019,14.625-6.058L171.268,369.98
+											<path className="mag_glass" fill="#FFFFFF" d="M6.057,505.942c4.038,4.039,9.332,6.058,14.625,6.058s10.587-2.019,14.625-6.058L171.268,369.98
 											c8.076-8.076,8.076-21.172,0-29.248c-8.076-8.078-21.172-8.078-29.249,0L6.057,476.693
 											C-2.019,484.77-2.019,497.865,6.057,505.942z"/>
 										</g>
@@ -110,12 +123,12 @@ const Header = () => (
 							</svg>
 						</div> */}
 
-            <div id="search_form" class="search_form bez_1">
-              <input type="search" class="search_content_input bez_1" />
+            <div id="search_form" className="search_form bez_1">
+              <input type="search" className="search_content_input bez_1" />
             </div>
 
-            <div class="hamburger">
-              <i class="fa fa-bars trans_200" />
+            <div className="hamburger">
+              <i className="fa fa-bars trans_200" />
             </div>
           </div>
         </div>
--- Saanwariya_WS/src/pages/index.js
+++ Saanwariya_WS/src/pages/index.js
@@ -8,9 +8,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
-    <Container >
-
-    </Container>
+  
   </div>
 )
 
