bb1e62c60d690757f7f17a3ebd7086c6df259d2d
yashaswi maharshi <maha.yash@gmail.com>
2018-09-04 06:48:45 +0530

    add styles & images from template

---
+++ Saanwariya_WS/src/pages/about-css-modules.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Container from '../components/container'
+//import styles from './about-css-modules.module.css'
+import styles from '../styles/about-css-modules.module.css'
+console.log(styles)
+
+export default () => (
+  <Container>
+    <h1>About CSS Modules</h1>
+    <p>CSS Modules are cool</p>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+  </Container>
+)
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
