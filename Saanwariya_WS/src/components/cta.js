a65287686068986bbbbbddfe0bec7f47c15b34a4
yashaswi maharshi <maha.yash@gmail.com>
2018-09-09 20:00:03 +0530

    places to visit

--- Saanwariya_WS/src/components/cta.js
+++ Saanwariya_WS/src/components/cta.js
@@ -4,7 +4,8 @@ import '../styles/main_styles.css'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-
+import Link from 'gatsby-link'
+import placesList from '../../content/ListOfVisitPlaces.json'
 
 class CTA extends React.Component {
   render() {
@@ -27,129 +28,48 @@ class CTA extends React.Component {
     }
 
     return (
-      <div className="cta">
-        <div
-          className="cta_background"
-          style={{ backgroundImage: 'url(images/cta.jpg)' }}
-        />
-
-        <div className="container">
-          <div className="row">
-            <div className="col">
+      <div>
+        <div className="cta">
+          <h1
+            className="text-center"
+            style={{ color: 'white', textTransform: 'uppercase' }}
+          >
+            Places to Visit
+          </h1>
+          <div className="container">
+            <div className="row">
               {/* <!-- CTA Slider --> */}
+              <div className="col">
+                <div className="cta_slider_container">
+                  <div className="owl-carousel owl-theme cta_slider">
+                    <Slider {...settings}>
+                      {placesList.map((data, index) => (
+                        <div
+                          className="owl-item cta_item text-center"
+                          key={index}
+                        >
+                          <div className="cta_title">{data.title}</div>
+                          <img
+                            src={`${data.imageUrl}`}
+                            style={{
+                              display: 'inline',
+                              marginTop: '20px',
+                              height: '250px',
+                            }}
+                          />
 
-              <div className="cta_slider_container">
-                <div className="owl-carousel owl-theme cta_slider">
-                  <Slider {...settings}>
-                    {/* <!-- CTA Slider Item --> */}
-                    <div className="owl-item cta_item text-center">
-                      <div className="cta_title">
-                        Vijay Vilas &nbsp;&nbsp; (Mandvi)
-                      </div>
-
-                      <p className="cta_text">
-                        {
-                          '1. We will show image of the place here, 2. 50 words description, 3. Read more'
-                        }
-                      </p>
-                      <div className="button cta_button">
-                        <div className="button_bcg" />
-                        <a href="#">
-                          Read More<span />
-                          <span />
-                          <span />
-                        </a>
-                      </div>
-                    </div>
-
-                    {/* <!-- CTA Slider Item --> */}
-                    <div className="owl-item cta_item text-center">
-                      <div className="cta_title">Swaminarayan Temple</div>
-
-                      <p className="cta_text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      </p>
-                      <div className="button cta_button">
-                        <div className="button_bcg" />
-                        <a href="#">
-                          book now<span />
-                          <span />
-                          <span />
-                        </a>
-                      </div>
-                    </div>
-
-                    {/* <!-- CTA Slider Item --> */}
-                    <div className="owl-item cta_item text-center">
-                      <div className="cta_title">Mandvi Beach</div>
-                      <p className="cta_text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam eu convallis tortor. Suspendisse potenti. In
-                      </p>
-                      <div className="button cta_button">
-                        <div className="button_bcg" />
-                        <a href="#">
-                          book now<span />
-                          <span />
-                          <span />
-                        </a>
-                      </div>
-                    </div>
-
-                    {/* <!-- CTA Slider Item --> */}
-                    <div className="owl-item cta_item text-center">
-                      <div className="cta_title">White Rann</div>
-
-                      <p className="cta_text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam eu convallis tortor. Suspendisse potenti. In
-                      </p>
-                      <div className="button cta_button">
-                        <div className="button_bcg" />
-                        <a href="#">
-                          book now<span />
-                          <span />
-                          <span />
-                        </a>
-                      </div>
-                    </div>
-
-                    {/* <!-- CTA Slider Item --> */}
-                    <div className="owl-item cta_item text-center">
-                      <div className="cta_title">Black Hill</div>
-
-                      <p className="cta_text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam eu convallis tortor. Suspendisse potenti. In
-                      </p>
-                      <div className="button cta_button">
-                        <div className="button_bcg" />
-                        <a href="#">
-                          book now<span />
-                          <span />
-                          <span />
-                        </a>
-                      </div>
-                    </div>
-
-                    {/* <!-- CTA Slider Item --> */}
-                    <div className="owl-item cta_item text-center">
-                      <div className="cta_title">Narayan Sarovar</div>
-                      <p className="cta_text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Nullam eu convallis tortor. Suspendisse potenti. In
-                      </p>
-                      <div className="button cta_button">
-                        <div className="button_bcg" />
-                        <a href="#">
-                          book now<span />
-                          <span />
-                          <span />
-                        </a>
-                      </div>
-                    </div>
-                  </Slider>
+                          <div className="button cta_button">
+                            <div className="button_bcg" />
+                            <Link to={data.link}>
+                              Read More<span />
+                              <span />
+                              <span />
+                            </Link>
+                          </div>
+                        </div>
+                      ))}
+                    </Slider>
+                  </div>
                 </div>
               </div>
             </div>
@@ -161,3 +81,85 @@ class CTA extends React.Component {
 }
 
 export default CTA
+
+{
+  /* <div className="owl-item cta_item text-center">
+                      <div className="cta_title">Swaminarayan Temple</div>
+
+                      <p className="cta_text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      </p>
+                      <div className="button cta_button">
+                        <div className="button_bcg" />
+                        <a href="#">
+                          Read More<span />
+                          <span />
+                          <span />
+                        </a>
+                      </div>
+                    </div>
+                    <div className="owl-item cta_item text-center">
+                      <div className="cta_title">Mandvi Beach</div>
+                      <p className="cta_text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nullam eu convallis tortor. Suspendisse potenti. In
+                      </p>
+                      <div className="button cta_button">
+                        <div className="button_bcg" />
+                        <a href="#">
+                          Read More<span />
+                          <span />
+                          <span />
+                        </a>
+                      </div>
+                    </div>
+                    <div className="owl-item cta_item text-center">
+                      <div className="cta_title">White Rann</div>
+
+                      <p className="cta_text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nullam eu convallis tortor. Suspendisse potenti. In
+                      </p>
+                      <div className="button cta_button">
+                        <div className="button_bcg" />
+                        <a href="#">
+                          Read More<span />
+                          <span />
+                          <span />
+                        </a>
+                      </div>
+                    </div>
+                    <div className="owl-item cta_item text-center">
+                      <div className="cta_title">Black Hill</div>
+
+                      <p className="cta_text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nullam eu convallis tortor. Suspendisse potenti. In
+                      </p>
+                      <div className="button cta_button">
+                        <div className="button_bcg" />
+                        <a href="#">
+                          Read More<span />
+                          <span />
+                          <span />
+                        </a>
+                      </div>
+                    </div>
+                    <div className="owl-item cta_item text-center">
+                      <div className="cta_title">Narayan Sarovar</div>
+                      <p className="cta_text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nullam eu convallis tortor. Suspendisse potenti. In
+                      </p>
+                      <div className="button cta_button">
+                        <div className="button_bcg" />
+                        <a href="#">
+                          Read More<span />
+                          <span />
+                          <span />
+                        </a>
+                      </div>
+                    </div>
+                  */
+}
