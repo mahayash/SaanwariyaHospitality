147b6ff7ed548b930d52620465f6d978304e6851
yashaswi maharshi <maha.yash@gmail.com>
2018-09-09 21:25:41 +0530

    minor changes in footer

--- Saanwariya_WS/src/components/footer.js
+++ Saanwariya_WS/src/components/footer.js
@@ -8,14 +8,14 @@ const Footer = () => (
       <div className="row">
         <div className="col-lg-12 order-lg-1 order-2  ">
           <div
-            className="copyright_content flex-row align-items-center"
-            style={{ marginTop: '20px' }}
+            className="copyright_content flex-row text-right"
+            style={{ marginTop: '25px' }}
           >
             <div>
               Copyright &copy;<script>
                 document.write(new Date().getFullYear());
               </script>{' '}
-              All rights reserved | powered by{' '}
+              All rights reserved | Powered by{' '}
               <a href="http://www.panthrax.com" target="_blank">
                 PDA
               </a>
